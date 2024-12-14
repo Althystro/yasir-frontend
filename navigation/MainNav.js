2e1975373ae5e1ad755eb3fa873eb166f052b6e4
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-12-14 20:55:07 +0300

    semi done

--- navigation/MainNav.js
+++ navigation/MainNav.js
@@ -1,9 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import AiRecomendation from "../components/AIRecomendation";
+import AllVehicles from "../components/AllVehicles";
 
 const MainNav = () => {
-  return <AiRecomendation />;
+  //return <AiRecomendation />;
+  return <AllVehicles />;
 };
 
 export default MainNav;
