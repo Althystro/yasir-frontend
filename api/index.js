e2b5883c42ff900241b0c129e43cdd312ed018a8
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-12-17 09:59:17 +0300

    index changed to have both urls

--- api/index.js
+++ api/index.js
@@ -2,9 +2,10 @@ import axios from "axios";
 import { getToken } from "./storage";
 
 // const BASE_URL = "https://react-native-food-delivery-be.eapi.joincoded.com/api";
+//  baseURL: "http://192.168.2.224:8080",
 
 const instance = axios.create({
-  baseURL: "http://192.168.2.224:8080",
+  baseURL: "http://localhost:8080",
 });
 instance.interceptors.request.use(async (config) => {
   const token = await getToken();
