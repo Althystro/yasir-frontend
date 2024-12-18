12a12e9e5e7cf81e784e8d77a208b9b02586158e
Nora <nhalmarri64@gmail.com>
2024-12-18 09:55:08 +0300

    done carbrands

--- data/carBrands.js
+++ data/carBrands.js
@@ -1,16 +1,32 @@
 const carBrands = [
-    { id: 1, brandName: "Porsche", brandIcon: "car" },
-    { id: 2, brandName: "Toyota", brandIcon: "car" },
-    { id: 3, brandName: "Ford", brandIcon: "car" },
-    { id: 4, brandName: "Mercedes", brandIcon: "car" },
-    { id: 5, brandName: "BMW", brandIcon: "car" },
-    { id: 6, brandName: "Audi", brandIcon: "car" },
-    { id: 7, brandName: "Chevrolet", brandIcon: "car" },
-    { id: 8, brandName: "Tesla", brandIcon: "car" },
-  ];
+  {
+    id: 1,
+    name: "Toyota",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Toyota_logo.png/512px-Toyota_logo.png",
+  },
+  {
+    id: 2,
+    name: "Ford",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Ford_logo_flat.svg/512px-Ford_logo_flat.svg.png",
+  },
+  {
+    id: 3,
+    name: "Mercedes-Benz",
+    logo: "https://cdn.freebiesupply.com/logos/large/2x/mercedes-benz-9-logo-png-transparent.png",
+  },
+  {
+    id: 4,
+    name: "Chevrolet",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Chevrolet_logo.png/512px-Chevrolet_logo.png",
+  },
+  {
+    id: 5,
+    name: "Porsche",
+    logo: "https://upload.wikimedia.org/wikipedia/en/thumb/8/8d/Porsche_logo.svg/512px-Porsche_logo.svg.png",
+  },
+];
 
-
-export default carBrands
+export default carBrands;
 
 
   
